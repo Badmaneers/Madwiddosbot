4904b047355c6c425c74c319ed5d8ea9b4d18d90
ZYLEX <86944731+Badmaneers@users.noreply.github.com>
2024-05-19 10:06:27 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -7,11 +7,11 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to the UDP Bot! Please use the command /run to start.")
+    bot.reply_to(message, "Welcome to the Madwiddos Bot! Please use the command /run to start.")
 
 @bot.message_handler(commands=['run'])
 def handle_run(message):
-    msg = bot.reply_to(message, "Please provide the following inputs separated by spaces:\n1. UDP\n2. IP:Port\n3. Thread\n4. Time")
+    msg = bot.reply_to(message, "Please provide the following inputs separated by spaces:1. IP:Port\n2. Thread\n3. Time")
     bot.register_next_step_handler(msg, process_inputs)
 
 def process_inputs(message):
@@ -20,13 +20,12 @@ def process_inputs(message):
         if len(inputs) != 4:
             raise ValueError("Invalid number of inputs.")
 
-        udp = inputs[0]
-        ip_port = inputs[1]
+        ip_port = inputs[0]
         thread = inputs[2]
-        time = inputs[3]
+        time = inputs[2]
 
         # Example command execution, adjust as necessary
-        command = f"python setup.py {udp} {ip_port} {thread} {time}"
+        command = f"python start.py udp {ip_port} {thread} {time}"
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
 
